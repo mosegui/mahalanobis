facdbdb2f8c961feb6150812c1837f991c899771
Daniel Moseguí González <mosegui@MUC.FOS4X.DE>
2019-09-15 13:43:57 +0200

    updated package setup file

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ import setuptools
 
 setuptools.setup(
      name='mahalanobis',
-     version='0.2',
+     version='1.0',
      packages=setuptools.find_packages(),
      author="Daniel Moseguí González",
      author_email="d.mosegui@gmail.com",
      description="Package for performing calculations of Mahalanobis distances",
      url="https://github.com/mosegui/mahalanobis",
      classifiers=[
-	 "Programming Language :: Python :: 3"
-         "Programming Language :: Python :: 3.6",
+	 "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
      ],
+    python_requires='>=3.6',
  )
