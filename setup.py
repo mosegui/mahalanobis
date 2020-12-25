51eee2d208ca5622e4a8955e8585a6e1f78f9190
mosegui <d.mosegui@gmail.com>
2020-12-25 20:27:50 +0100

    add project description for PyPi repo

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
+import os
 import setuptools
+
+_readme_path = os.path.join(os.path.dirname(__file__), "README.md")
+
+with open(_readme_path) as readme:
+    long_description = readme.read()
 
 setuptools.setup(
      name='mahalanobis',
-     version='1.0.1',
+     version='1.2.0',
      packages=setuptools.find_packages(),
      author="Daniel Moseguí González",
      author_email="d.mosegui@gmail.com",
      description="Package for performing calculations of Mahalanobis distances",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
      url="https://github.com/mosegui/mahalanobis",
      classifiers=[
 	 "Programming Language :: Python :: 3",
